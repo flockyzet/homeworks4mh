a78aa61701ff561c628a105251f3769ea74a0217
flockyzet <gtx25427@gmail.com>
2026-09-08 02:37:04 +0600

    home work 1

---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+# Create your views here.
+
+
+def hello_world(r):
+    return HttpResponse("<h1>Hello World!</h1>")
+
+
+def my_name(r):
+    name = "Anvar"
+
+    return HttpResponse(f"<h2 Hello </h2> <h1> {name} </h1>")
+
+
+def say_name(r, name):
+    return HttpResponse(f"<h2 Hello </h2> <h1> {name} </h1>")
